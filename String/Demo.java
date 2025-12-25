90625d1921732fd5fc2e449fc5ba15c62f56557f
Piyush kushwah <piyushkushwah002@gmail.com>
2025-12-25 12:21:59 +0530

    Add files via upload

---
+++ String/Demo.java
@@ -0,0 +1,36 @@
+package String;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+//		StringBuilder sb1 = new StringBuilder("Prasoon");
+//		StringBuilder sb2 = new StringBuilder("Prasoon");
+//		StringBuilder sb3 = new StringBuilder("Prasoon");
+//		StringBuilder sb4 = new StringBuilder("Prasoon");
+//		
+//		System.out.println(sb1.equals(sb2));
+//		System.out.println(sb1.equals(sb3));
+//		System.out.println(sb1.equals(sb4));
+//		System.out.println(sb2.equals(sb3));
+//		System.out.println(sb2.equals(sb4));
+//		System.out.println(sb3.equals(sb4));
+//		
+//		System.out.println(sb1==sb2);
+//		System.out.println(sb1==sb3);
+//		System.out.println(sb1==sb4);
+//		System.out.println(sb2==sb3);
+//		System.out.println(sb2==sb4);
+//		System.out.println(sb3==sb4);
+//		
+//		System.out.println(sb1.hashCode());
+//		System.out.println(sb2.hashCode());
+//		System.out.println(sb3.hashCode());
+//		System.out.println(sb4.hashCode());
+		
+//		String s= "java";
+//		s=s+"prog";
+//		System.out.println(s);
+		
+	}
+
+}
---
+++ String/DemoString.java
@@ -0,0 +1,41 @@
+package String;
+
+public class DemoString {
+	public static void main(String[] args) {
+		   
+		// String 
+//		  String s = new String("Java");
+//		  s.concat("programming");
+//		  System.out.println(s);
+		
+		// String Buffer 
+//		  StringBuffer sb= new StringBuffer("Java");
+//		  sb.append("Programming");
+//		  System.out.println(sb);
+		
+		// String Builder
+//		  StringBuilder sb = new StringBuilder("Java");
+//		  sb.append("Programming");
+//		  System.out.println(sb);
+		
+		// String Literal
+//		 String s1 = "Java";     
+//		 String s2 = "Java";
+//		 String s3 = "Java";
+		// New String
+//		 String s4 = new String("Java");
+//		 String s5 = new String("Java");
+//		 String s6 = new String("Java");	 
+		 
+		
+//		 System.out.println(s1==s2);
+//		 System.out.println(s2==s3);	 
+//		 System.out.println(s1.equals(s2));
+//		 System.out.println(s2.equals(s3)); 
+//		 System.out.println(s4==s5);
+//		 System.out.println(s5==s6);
+//		 System.out.println(s4.equals(s5));
+//		 System.out.println(s5.equals(s6));
+		
+	}
+}
