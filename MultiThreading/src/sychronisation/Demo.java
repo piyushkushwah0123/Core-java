866b34c0a14384c03b9aafd513f90fa7ae020b7a
Piyush kushwah <piyushkushwah002@gmail.com>
2026-04-14 12:00:08 +0530

    Add files via upload

--- MultiThreading/src/sychronisation/Demo.java
+++ MultiThreading/src/sychronisation/Demo.java
@@ -1,38 +1,39 @@
 package sychronisation;
-// Synchronization very very important  { Restrict kar dena }
-class Student implements Runnable{
+
+// Synchronization very very important  { Restrict kr dena }
+class Student implements Runnable {
 	@Override
-	public synchronized void run() {  // Method Level Lock  // Method Synchronization	
+	public synchronized void run() { // Method Level Lock // Method Synchronization
 		try {
 			System.out.println(Thread.currentThread().getName() + "classroom se bahr gya"); // Name
 			Thread.sleep(3000); // Checked Exception [ Interrupted exception ]
-			System.out.println(Thread.currentThread().getName() + "Notice board wale Area mai enter hua " );
+			System.out.println(Thread.currentThread().getName() + "Notice board wale Area mai enter hua ");
 			Thread.sleep(3000);
 			System.out.println(Thread.currentThread().getName() + "Notice Board par jo notice likha hua tha usko padha ");
 			Thread.sleep(3000);
-			System.out.println(Thread.currentThread().getName() + "Notice padhne ke baad Institue se bahr nikal gya");	
-		}
-		catch(InterruptedException e) {
+			System.out.println(Thread.currentThread().getName() + "Notice padhne ke baad Institue se bahr nikal gya");
+		} catch (InterruptedException e) {
 			System.out.println("Some Problems........");
-		}	
+		}
 	}
 }
+
 public class Demo {
 	public static void main(String[] args) {
-		
+
 		Student c = new Student();
-		
+
 		Thread t1 = new Thread(c);
 		Thread t2 = new Thread(c);
 		Thread t3 = new Thread(c);
-		
+
 		t1.setName("Student-1");
 		t2.setName("Student-2");
 		t3.setName("Student-3");
-		
+
 		t1.start();
 		t2.start();
-		t3.start();		
+		t3.start();
 	}
 }
 
@@ -44,7 +45,7 @@ public class Demo {
 //		System.out.println(Thread.currentThread().getName() + "Came in Parking Alot"); // Name
 //		Thread.sleep(3000); // Checked Exception [ Interrupted exception ]
 
-   //   synchronization(this){
+// synchronization(this){
 //		System.out.println(Thread.currentThread().getName() + "Come for Drive the Car" );
 //		Thread.sleep(3000);
 //		System.out.println(Thread.currentThread().getName() + "Started to Drive the car");
@@ -63,21 +64,18 @@ public class Demo {
 //public class Demo {
 //public static void main(String[] args) {
 //	
-//	Son c = new Son();
+//	    Son c = new Son();
 //	
-//	Thread t1 = new Thread(c);
-//	Thread t2 = new Thread(c);
-//	Thread t3 = new Thread(c);
+//	    Thread t1 = new Thread(c);
+//	    Thread t2 = new Thread(c);
+//      Thread t3 = new Thread(c);
 //	
-//	t1.setName("Son-1");
-//	t2.setName("Son-2");
-//	t3.setName("Son-3");
+//	    t1.setName("Son-1");
+//	    t2.setName("Son-2");
+//	    t3.setName("Son-3");
 //	
-//	t1.start();
-//	t2.start();
-//	t3.start();
-//	
-//}
-//
-//
+//	    t1.start();
+//	    t2.start();
+//	    t3.start();
+//	  }
 //}
